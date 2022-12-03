47843285edea48020bc6263e11c894740692c0e4
Hob Spillane <hob.spillane@workday.com>
2022-12-03 01:53:10 -0500

    day 3 challenge 2 works

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -12,6 +12,31 @@ var p1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var p2 = "abcdefghijklmnopqrstuvwxyz"
 
 func main() {
+	findBadges()
+}
+
+func findBadges() {
+	file, err := os.Open("cmd/day3/input.txt")
+	defer file.Close()
+
+	util.CheckErr(err)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	group := make([]string, 0)
+	totalPriority := 0
+	for scanner.Scan() {
+		group = append(group, scanner.Text())
+		if len(group) == 3 {
+			s := findCharsInCommon(group)
+			totalPriority += getItemTypePriority(s)
+			//Reset for the next group
+			group = make([]string, 0)
+		}
+	}
+	println(fmt.Sprintf("Total Priority: %d", totalPriority))
+}
+
+func findMisplacedItems() {
 	file, err := os.Open("cmd/day3/input.txt")
 	defer file.Close()
 
@@ -23,23 +48,30 @@ func main() {
 		contents := scanner.Text()
 		compartment1 := contents[0 : len(contents)/2]
 		compartment2 := contents[len(contents)/2 : len(contents)]
-		c := findCharsInCommon(compartment1, compartment2)
-		if strings.Contains(p1, c) {
-			p := strings.Index(p1, c) + 1 + 26
-			totalPriority += p
-		} else {
-			p := strings.Index(p2, c) + 1
-			totalPriority += p
-		}
+		c := findCharsInCommon([]string{compartment1, compartment2})
+		totalPriority += getItemTypePriority(c)
 	}
 	println(fmt.Sprintf("Total Priority: %d", totalPriority))
 }
 
-func findCharsInCommon(compartment1 string, compartment2 string) string {
-	for _, c := range compartment1 {
-		s := string(c)
-		if strings.Contains(compartment2, s) {
-			return s
+func getItemTypePriority(t string) int {
+	if strings.Contains(p1, t) {
+		return strings.Index(p1, t) + 1 + 26
+	} else {
+		return strings.Index(p2, t) + 1
+	}
+}
+
+func findCharsInCommon(lists []string) string {
+	for _, c := range lists[0] {
+		theRest := lists[1:len(lists)]
+		for i, s := range theRest {
+			if !strings.Contains(s, string(c)) {
+				break
+			}
+			if i == len(theRest)-1 {
+				return string(c)
+			}
 		}
 	}
 	return ""
