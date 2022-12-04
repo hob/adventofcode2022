7e5a6e4dbbf62227fed71874f79d1f17412a7112
Hob Spillane <hob.spillane@workday.com>
2022-12-04 09:39:23 -0500

    day 4 challenge 2 works

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -18,15 +18,21 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 	numFullyContained := 0
 	for scanner.Scan() {
-		assignments := strings.Split(scanner.Text(), ",")
+		line := scanner.Text()
+		print(line)
+		assignments := strings.Split(line, ",")
 		a1 := strings.Split(assignments[0], "-")
 		a2 := strings.Split(assignments[1], "-")
 		a1Start, _ := strconv.Atoi(a1[0])
 		a1End, _ := strconv.Atoi(a1[1])
 		a2Start, _ := strconv.Atoi(a2[0])
 		a2End, _ := strconv.Atoi(a2[1])
-		if (a1Start <= a2Start && a1End >= a2End) || (a2Start <= a1Start && a2End >= a1End) {
+		if (a1Start >= a2Start && a1Start <= a2End) || (a1End >= a2Start && a1End <= a2End) ||
+			(a2Start >= a1Start && a2Start <= a1End) || (a2End >= a1Start && a2End <= a1End) {
+			println(" - Yup")
 			numFullyContained++
+		} else {
+			println("")
 		}
 	}
 	println(fmt.Sprintf("Number of assignments with full overlap: %d", numFullyContained))
