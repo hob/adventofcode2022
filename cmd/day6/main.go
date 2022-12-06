c71fe2f81697a1ce425c8bc817dee9bec8598efe
Hob Spillane <hob.spillane@gmail.com>
2022-12-06 00:47:47 -0500

    Day 6 challenge 1 works

---
+++ cmd/day6/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"advent22.spillane.farm/internal/util"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("cmd/day6/input.txt")
+	defer file.Close()
+
+	util.CheckErr(err)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	scanner.Scan()
+	line := scanner.Text()
+	i := 0
+	for !allUniqueChars(line[i : i+4]) {
+		i++
+	}
+	println(fmt.Sprintf("Uniquness occurred at character %d with characters %s", i+4, line[i:i+4]))
+}
+
+func allUniqueChars(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if strings.Count(s, string(s[i])) > 1 {
+			return false
+		}
+	}
+	return true
+}
